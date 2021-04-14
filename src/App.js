e4faf89b33669e582d2d05ccd70c903dc15a7c6d
theindianappguy <theindianappguy@gmail.com>
2021-04-14 22:02:55 +0530

    added paths via react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import HomePage from "./pages/home";
+import SubmitEventPage from "./pages/submit";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <div>
-      <HomePage />
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <HomePage />
+          </Route>
+          <Route exact path='/submit'>
+            <SubmitEventPage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
