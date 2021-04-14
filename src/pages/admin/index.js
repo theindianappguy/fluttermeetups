af0650db94f351b30c4a439bf54eba54d1512a08
theindianappguy <theindianappguy@gmail.com>
2021-04-14 23:47:44 +0530

    check if not admin send to home

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { UserContext } from "../../contexts/user-context";
 
 export default function AdminPage() {
+  const [user, setUser] = useContext(UserContext).user;
+  let history = useHistory();
+
+  function sendToUrl(string) {
+    history.push(string);
+  }
+
+  //check if admin email if not send to home.
+  if (user?.uid !== "N5siU8Wi1OXoLg6TtjNxHp4tOIl2") sendToUrl("/");
+
   return (
     <div>
       <p>Admin Page</p>
