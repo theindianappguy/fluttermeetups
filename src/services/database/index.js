6f8bc539a4bcf62c66123bdd8ad19ee497d089de
theindianappguy <theindianappguy@gmail.com>
2021-04-15 01:07:25 +0530

    added approve and reject & delete option for admin

--- src/services/database/index.js
+++ src/services/database/index.js
@@ -18,6 +18,27 @@ export const getEvents = async () => {
 
   await db
     .collection("events")
+    .where("verified", "==", true)
+    //.orderBy("date", "desc")
+    .limit(5)
+    .get()
+    .catch(function (error) {})
+    .then(function (querySnapshot) {
+      querySnapshot.forEach(function (doc) {
+        fetched_events.push({ id: doc.id, event: doc.data() });
+
+        last_visible_event_doc = doc.data;
+      });
+    });
+  return [fetched_events, last_visible_event_doc];
+};
+
+export const getAdminEvents = async () => {
+  let fetched_events = [];
+  let last_visible_event_doc;
+
+  await db
+    .collection("events")
     .orderBy("date", "desc")
     .limit(5)
     .get()
@@ -30,4 +51,14 @@ export const getEvents = async () => {
       });
     });
   return [fetched_events, last_visible_event_doc];
+};
+
+export const toggleEventVerification = (eventId, verificationStatus) => {
+  db.collection("events")
+    .doc(eventId)
+    .set({ verified: !verificationStatus }, { merge: true });
+};
+
+export const deleteEvent = (eventId) => {
+  db.collection("events").doc(eventId).delete();
 };
