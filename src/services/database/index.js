d26aa4b4505921c9d89aeeb0c7dc6221d098870a
theindianappguy <theindianappguy@gmail.com>
2021-04-15 00:12:14 +0530

    created function to get events

--- src/services/database/index.js
+++ src/services/database/index.js
@@ -11,3 +11,23 @@ export const uploadUserInfo = (userId, name, email, avatarUrl) => {
 export const uploadEventInfo = async (userId, eventMap) => {
   await db.collection("events").doc(userId).set(eventMap);
 };
+
+export const getEvents = async () => {
+  let fetched_events = [];
+  let last_visible_event_doc;
+
+  await db
+    .collection("events")
+    .orderBy("date", "desc")
+    .limit(5)
+    .get()
+    .catch(function (error) {})
+    .then(function (querySnapshot) {
+      querySnapshot.forEach(function (doc) {
+        fetched_events.push({ id: doc.id, event: doc.data() });
+
+        last_visible_event_doc = doc.data;
+      });
+    });
+  return [fetched_events, last_visible_event_doc];
+};
