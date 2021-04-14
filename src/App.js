ccf355ce116c538c82b299c85903e56d78966330
theindianappguy <theindianappguy@gmail.com>
2021-04-14 23:47:24 +0530

    set user from local to context api

--- src/App.js
+++ src/App.js
@@ -2,8 +2,21 @@ import HomePage from "./pages/home";
 import SubmitEventPage from "./pages/submit";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import AdminPage from "./pages/admin";
+import { useContext, useEffect } from "react";
+import { UserContext } from "./contexts/user-context";
 
 export default function App() {
+  const [user, setUser] = useContext(UserContext).user;
+
+  useEffect(() => {
+    if (localStorage.getItem("user")) {
+      try {
+        setUser(JSON.parse(localStorage.getItem("user")));
+      } catch (error) {
+        setUser(null);
+      }
+    }
+  }, []);
   return (
     <div>
       <Router>
@@ -15,7 +28,7 @@ export default function App() {
             <SubmitEventPage />
           </Route>
           <Route exact path='/admin'>
-            <AdminPage />
+            {user ? <AdminPage /> : <></>}
           </Route>
         </Switch>
       </Router>
