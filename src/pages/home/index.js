2011c77612017bc38f92f2909780790f05779265
theindianappguy <theindianappguy@gmail.com>
2021-04-15 11:43:11 +0530

    infinite scroll + convertkit api added

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,17 +1,26 @@
 import React, { useContext, useEffect, useState } from "react";
 import EventTile from "../../components/event-tile";
 import "./index.css";
+import { v4 as uuidv4 } from "uuid";
 
 import { signInWithGoogle } from "../../services/auth";
 import { UserContext } from "../../contexts/user-context";
 import { useHistory } from "react-router-dom";
 
-import { getEvents } from "../../services/database";
+import { getEvents, getMoreEvents } from "../../services/database";
 import { toDateTime } from "../../funcions";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default function HomePage() {
   const [user, setUser] = useContext(UserContext).user;
   const [events, setevents] = useState([]);
+  const [hasmore, sethasmore] = useState(true);
+  const [lastVisible, setlastVisible] = useState();
+
+  const [emailAddress, setemailAddress] = useState("");
+  const [messageStr, setmessageStr] = useState("");
+
+  const [formLoading, setformLoading] = useState(false);
 
   let history = useHistory();
 
@@ -29,18 +38,147 @@ export default function HomePage() {
     console.log(`user we get on signin : ${user}`);
   };
 
-  const getSetEvents = async () => {
-    let [fetchedEvents, _last] = await getEvents();
-    console.log(`these are the fetched ideas: ${fetchedEvents}`);
-    setevents(fetchedEvents);
+  const fetchEvents = async () => {
+    let [fetchedEvents, lastVisibleLocal] = lastVisible
+      ? await getMoreEvents(lastVisible)
+      : await getEvents();
+    console.log(
+      `these are the fetched ideas: ${fetchedEvents} fetchedEvents length:${fetchedEvents.length}, lastEventName:${lastVisibleLocal.event["name"]}`
+    );
+    setevents([...events, ...fetchedEvents]);
+    setlastVisible(lastVisibleLocal);
+
+    if (fetchedEvents.length === 0) {
+      sethasmore(false);
+    }
   };
 
   useEffect(() => {
-    getSetEvents();
+    fetchEvents();
   }, []);
+
+  const SubmitEmailConvertKit = (e) => {
+    e.preventDefault();
+    setformLoading(true);
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+      email: emailAddress,
+      api_key: "UfP8hppSlI4ODe-gV9ic0g",
+    });
+
+    setemailAddress("");
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://api.convertkit.com/v3/forms/2202312/subscribe",
+      requestOptions
+    )
+      .then((response) => response.text())
+      .then((result) => {
+        console.log(result);
+        setmessageStr(
+          "Success! Now check your email to confirm your subscription."
+        );
+        setformLoading(false);
+      })
+      .catch((error) => console.log("error", error));
+  };
 
   return (
     <div className='homePage'>
+      {/* Header */}
+      <header>
+        <div
+          className='container'
+          style={{
+            alignItems: "center",
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <div className='header__logo'>
+            <div className='header__logo__img'>
+              <a
+                style={{
+                  textDecoration: "none",
+                  color: "#111",
+                  fontSize: "18px",
+                }}
+                href='https://fluttermeetups.web.app/'
+              >
+                Flutter Meetups
+              </a>
+            </div>
+          </div>
+          <div className='header__menu'>
+            <div className='header__menu__items show-for-medium-up'>
+              <a
+                href='https://www.sanskar.dev'
+                style={{
+                  marginRight: "26px",
+                  cursor: "pointer",
+                  textDecoration: "none",
+                  color: "#111",
+                  fontSize: "18px",
+                  cursor: "pointer",
+                }}
+              >
+                Made by
+              </a>
+              <a
+                href='https://flutternerd.com'
+                style={{
+                  marginRight: "26px",
+                  cursor: "pointer",
+                  textDecoration: "none",
+                  color: "#111",
+                  fontSize: "18px",
+                  cursor: "pointer",
+                }}
+              >
+                Flutter Blogs
+              </a>
+              <a
+                href='https://www.youtube.com/channel/UCsPdgUIoOBTBI1UmulW1pdw'
+                style={{
+                  marginRight: "26px",
+                  cursor: "pointer",
+                  textDecoration: "none",
+                  color: "#111",
+                  fontSize: "18px",
+                  cursor: "pointer",
+                }}
+              >
+                Flutter Tutorials
+              </a>
+            </div>
+          </div>
+
+          <div>
+            <a
+              href='https://github.com/theindianappguy/fluttermeetups'
+              style={{
+                textDecoration: "none",
+                color: "#111",
+                fontSize: "18px",
+              }}
+            >
+              Github
+            </a>
+          </div>
+        </div>
+      </header>
+
+      {/* Hero */}
       <div className='container'>
         <div className='hero__title'>An Open List of Flutter Events</div>
         <h4 className='hero__subtitle'>
@@ -56,10 +194,36 @@ export default function HomePage() {
             alignItems: "center",
           }}
         >
-          <div style={{ display: "flex", alignItems: "center" }}>
-            <input className='hero__ctaInput' placeholder='Email address' />
-            <button className='hero_ctaButton'>Get Updates</button>
-          </div>
+          {messageStr !== "" && (
+            <p className='success__message'>{messageStr}</p>
+          )}
+          {formLoading ? (
+            <div className='loading__div'>
+              <p>Loading...</p>
+            </div>
+          ) : (
+            <form
+              onSubmit={(e) => {
+                SubmitEmailConvertKit(e);
+              }}
+              style={{ display: "flex", alignItems: "center" }}
+            >
+              <input
+                type='email'
+                onChange={(e) => setemailAddress(e.target.value)}
+                className='hero__ctaInput'
+                placeholder='Email address'
+                value={emailAddress}
+              />
+              <button type='submit' className='hero_ctaButton'>
+                Get Updates
+              </button>
+            </form>
+          )}
+
+          <p style={{ fontSize: "14px", marginTop: "6px", color: "grey" }}>
+            We respect your privacy. Unsubscribe at any time sngle click.
+          </p>
 
           <p className='here__ctaSubmitEvent' onClick={onSignInBtnClick}>
             Submit Event
@@ -71,10 +235,24 @@ export default function HomePage() {
           latest updates ultimately making you a better web developer
         </p>
 
-        <div style={{ marginTop: "32px" }}>
+        <InfiniteScroll
+          dataLength={events.length} //This is important field to render the next data
+          next={fetchEvents}
+          hasMore={hasmore}
+          loader={
+            <div className='loading__div'>
+              <p>Loading...</p>
+            </div>
+          }
+          endMessage={
+            <p style={{ textAlign: "center" }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
+        >
           {events.map(({ id, event }) => {
             return (
-              <div key={event["name"]}>
+              <div key={uuidv4()}>
                 <EventTile
                   title={event["name"]}
                   desc={event["desc"]}
@@ -83,7 +261,11 @@ export default function HomePage() {
               </div>
             );
           })}
-        </div>
+        </InfiniteScroll>
+
+        {/* <div style={{ marginTop: "32px" }}>
+          
+        </div> */}
       </div>
     </div>
   );
